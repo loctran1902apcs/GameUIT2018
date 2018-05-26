fcdf5398fbf7ce0d033610d2735273c94842b0d3
Nghi <pthai.nghi@gmail.com>
2018-05-26 14:54:11 +0700

    Add picture view

---
+++ src/main/java/apcs/chillme/PictureInfo.java
@@ -0,0 +1,20 @@
+package com.example.admin.pictures;
+
+public class PictureInfo {
+    int picresource;
+    String picName;
+
+    public PictureInfo(String name,int res)
+    {
+        this.picName=name;
+        picresource=res;
+    }
+
+    public int getPicresource() {
+        return picresource;
+    }
+
+    public String getPicName() {
+        return picName;
+    }
+}
