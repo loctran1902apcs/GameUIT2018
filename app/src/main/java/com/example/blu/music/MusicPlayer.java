22e2ba5c6da8b441a4a7851ed6fdd200ac6a34ff
axblueblader <blueassassinx@gmail.com>
2018-05-26 14:42:24 +0700

    music 1

---
+++ app/src/main/java/com/example/blu/music/MusicPlayer.java
@@ -0,0 +1,41 @@
+package com.example.blu.music;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class MusicPlayer{
+    private MediaPlayer mPlayer;
+    private Context context;
+
+    MusicPlayer(Context context){
+        mPlayer = new MediaPlayer();
+        this.context = context;
+    }
+
+    public void loadMedia(int resourceId) {
+        mPlayer = MediaPlayer.create(context,resourceId);
+    }
+
+    public void release() {
+        mPlayer.release();
+    }
+
+
+    public boolean isPlaying() {
+        return mPlayer.isPlaying();
+    }
+
+    public void play() {
+        mPlayer.start();
+    }
+
+    public void reset() {
+        mPlayer.reset();
+    }
+
+    public void pause() {
+        if (mPlayer.isPlaying())
+            mPlayer.pause();
+        }
+
+}
